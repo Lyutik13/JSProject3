667f9e1bf1d4c1fc638995281b47d40c88b0aec2
Lyutik13 <lyutikov.viktor.96@mail.ru>
2023-06-07 21:20:33 +0300

    vodeo

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
 'use strict'
 import Slider from './modules/slider'
+import VideoPlayer from './modules/playVideo'
 
 window.addEventListener('DOMContentLoaded', () => {
 	const slider = new Slider('.page', '.next')
 	slider.render()
+
+  const videoPlayer = new VideoPlayer('.showup .play', '.overlay')
+  videoPlayer.init()
 })
 
 // json-server src/db.json
